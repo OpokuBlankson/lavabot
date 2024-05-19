3438c7edbbea545b143ba632bde04b7a1f6468a8
OpokuBlankson <56057458+OpokuBlankson@users.noreply.github.com>
2024-05-19 15:07:00 +0000

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-b4a763db-d326-49f6-b7ba-a76991e538b6/"
 
 OUTPUT_DIR="logs"
 
